b7c17bb2f9fc99a53a43673819e6d1dd81d445ba
Olivier <olivier.moreau0@laposte.net>
2020-10-06 01:26:31 +0200

    commit before restructuring forlders

---
+++ reader.py
@@ -0,0 +1,10 @@
+import pickle
+
+def grid_picker(g, r):
+    'pick a grid from the file, g = the grid, r = one of 4 90° rotations'
+    with open("grids.data", "rb") as f:
+        content= pickle.load(f)
+        grid_ret = content[g][0][r]
+        full_grid_ret = content[g][1][r]
+
+        return [grid_ret, full_grid_ret]
